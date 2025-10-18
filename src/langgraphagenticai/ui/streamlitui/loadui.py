d3307b80df26ab49d6515d40ee0a6d59ae0793da
Sunny Farooq <msaneefarooq34@yahoo.com>
2025-10-18 15:38:01 +0500

    changes

--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -9,15 +9,28 @@ class LoadStreamlitUI:
         self.user_controls={}
 
     def load_streamlit_ui(self):
-        st.set_page_title(page_title= "🤖" + self.config.get_page_title(), layout="wide")
-        st.header("🤖" + self.config.get_page_title())
+        st.set_page_config(page_title= "🤖 " + self.config.get_page_title(), layout="wide")
+        st.header("🤖 " + self.config.get_page_title())
+
 
         with st.sidebar:
             # Get options from config
             llm_options = self.config.get_llm_options()
             usecase_options = self.config.get_usecase_options()
 
-            self.user_controls["selected_llm"] = st.select_box("Select LLM", llm_options)
+            # LLM selection
+            self.user_controls["selected_llm"] = st.selectbox("Select LLM", llm_options)
 
             if self.user_controls["selected_llm"] == 'Groq':
-                model_options = self.confg.get_groq_model_options()
+                # Model selection
+                model_options = self.config.get_groq_model_options()
+                self.user_controls["selected_groq_model"] = st.selectbox("Select Model", model_options)
+                self.user_controls["GROQ_API_KEY"] = st.session_state["GROQ_API_KEY"]=st.text_input("API Key",type="password")
+                # Validate API key
+                if not self.user_controls["GROQ_API_KEY"]:
+                    st.warning("⚠️ Please enter your GROQ API key to proceed. Don't have? refer : https://console.groq.com/keys ")
+            
+            ## USecase selection
+            self.user_controls["selected_usecase"]=st.selectbox("Select Usecases",usecase_options)
+
+        return self.user_controls
--- src/langgraphagenticai/ui/uiconfigfile.py
+++ src/langgraphagenticai/ui/uiconfigfile.py
@@ -1,7 +1,8 @@
 from configparser import ConfigParser
 
+
 class Config:
-    def __init__(self, config_file="./src/langgraphagenticai/ui/uiconfigfile.ini"):
+    def __init__(self,config_file="./src/langgraphagenticai/ui/uiconfigfile.ini"):
         self.config=ConfigParser()
         self.config.read(config_file)
 
@@ -10,7 +11,7 @@ class Config:
     
     def get_usecase_options(self):
         return self.config["DEFAULT"].get("USECASE_OPTIONS").split(", ")
-    
+
     def get_groq_model_options(self):
         return self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS").split(", ")
     
